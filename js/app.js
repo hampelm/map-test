c3293f9872284f0de11ca9c2c57c36a74f37d0a7
Matt Hampel <matth@localdata.com>
2014-04-22 09:05:42 -0400

    Add better logging

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(function(){
   var map = L.map('map').setView([42.42, -83.02 ], 13);
 
-  baseLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/matth.map-zmpggdzn/{z}/{x}/{y}.png');
+  var baseLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/matth.map-zmpggdzn/{z}/{x}/{y}.png');
   map.addLayer(baseLayer);
 
   function onLocationFound(e) {
+    console.log("Found location:", e.latlng.lat, e.latlng.lng);
+    alert("Found location: " + e.latlng.lat + " " + e.latlng.lng);
+
     var radius = e.accuracy / 2;
 
     L.marker(e.latlng).addTo(map)
@@ -13,6 +16,11 @@ $(function(){
     L.circle(e.latlng, radius).addTo(map);
   }
   map.on('locationfound', onLocationFound);
+  map.on('locationerror', function(error) {
+    console.log("Error finding location", error);
+    alert("Location error: " + error);
+  });
+
   map.locate({setView: true, maxZoom: 18});
 
 
